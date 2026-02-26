b14e42959b13a24918b4b01e285c60556f804700
MorMai <104486039+MorMai@users.noreply.github.com>
2026-02-26 11:38:02 +0700

    Add Knockback

--- Assets/Script/Component/Attack.cs
+++ Assets/Script/Component/Attack.cs
@@ -1,8 +1,12 @@
 using Unity.VisualScripting.Antlr3.Runtime.Misc;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
 public class Attack : MonoBehaviour, IRequireStats
 {
+    [SerializeField] private float knockbackForce = 10f;
+    [SerializeField] private float knockbackVerticalBoost = 2f; // Adds a little upward
+
     private float minDamage;
     private float maxDamage;
     public void Initialize(EntityData stats)
@@ -18,6 +22,21 @@ public class Attack : MonoBehaviour, IRequireStats
         {
             damageable.TakeDamage(attackDamage);
             Debug.Log($"Attacked {target.name} for {attackDamage} damage.");
+        }
+
+        ApplyKnockback(target);
+    }
+
+    private void ApplyKnockback (GameObject target)
+    {
+        Rigidbody2D rb = target.GetComponent<Rigidbody2D>();
+
+        if (rb != null)
+        {
+            // Calculate direction: Target position - Attacker position
+            Vector2 direction = (target.transform.position - transform.position).normalized;
+            direction.y += knockbackVerticalBoost;
+            rb.AddForce(direction * knockbackForce, ForceMode2D.Impulse);
         }
     }
 }
