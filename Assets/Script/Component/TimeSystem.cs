973f26aded62cab2d92629caed1e73f3b1d5cf7f
MorMai <104486039+MorMai@users.noreply.github.com>
2026-02-26 23:36:21 +0700

    Update Time System Component

--- Assets/Script/Component/TimeSystem.cs
+++ Assets/Script/Component/TimeSystem.cs
@@ -1,8 +1,35 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TimeSystem : MonoBehaviour
 {
-    
+    public int CurrentDayIndex { get; private set; } = 0;
+    private int _currentPeriodIndex = 0;
+    public DayPeriod CurrentPeriod => (DayPeriod)_currentPeriodIndex;
+    public WeekDay CurrentWeekDay => (WeekDay)(CurrentDayIndex % totalWeekDays);
+
+    public event Action<DayPeriod, WeekDay, int> OnTimeChanged;
+
+    private int totalPeriods;
+    private int totalWeekDays;
+
+    private void Awake()
+    {
+        totalPeriods = Enum.GetValues(typeof(DayPeriod)).Length;
+        totalWeekDays = Enum.GetValues(typeof(WeekDay)).Length;
+    }
+    public void AdvanceTime()
+    {
+        _currentPeriodIndex++;
+
+        if (_currentPeriodIndex >= totalPeriods)
+        {
+            _currentPeriodIndex = 0;
+            CurrentDayIndex++;
+        }
+
+        OnTimeChanged?.Invoke(CurrentPeriod, CurrentWeekDay, CurrentDayIndex);
+    }
 }
