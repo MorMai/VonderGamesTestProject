c1074972926ac4494fedf2893dbece2ccbe1a174
MorMai <104486039+MorMai@users.noreply.github.com>
2026-02-26 23:37:42 +0700

    Time hop test

--- Assets/Script/Controller/TimeHopTrigger.cs
+++ Assets/Script/Controller/TimeHopTrigger.cs
@@ -2,5 +2,12 @@ using UnityEngine;
 
 public class TimeHopTrigger : MonoBehaviour
 {
-   
+    [SerializeField] private TimeSystem timeSystem;
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            timeSystem.AdvanceTime();
+        }
+    }
 }
